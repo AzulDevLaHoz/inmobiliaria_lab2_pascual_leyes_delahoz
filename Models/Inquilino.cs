2fbcac797e8294052bbac79166cd96dca264db4d
ClaveroJuanSL <juan.clavero24@gmail.com>
2026-08-14 22:18:58 -0300

    Revert "se agregaron modelos"
    This reverts commit 5c3a209593a855d26e52d6001078886827e67543.

--- Models/Inquilino.cs
+++ Models/Inquilino.cs
@@ -9,7 +9,7 @@ namespace inmobiliaria_lab2_pascual_leyes_delahoz_clavero.Models
     {
         [Key]
         [Display(Name = "Código Int.")]
-        public int IdInquilino { get; set; }
+        public int idInquilino { get; set; }
 
         [Required]
         public String Nombre { get; set; } = "";

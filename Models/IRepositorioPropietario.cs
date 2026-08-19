da27924ce271f43d0525eafe054b68746bbc04a5
Luza Dev <azuldelahoz@gmail.com>
2026-08-19 01:47:25 -0300

    IRepoProp
    Acomode un detalle del namespace

--- Models/IRepositorioPropietario.cs
+++ Models/IRepositorioPropietario.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Inmobiliaria_.Net_Core.Models; //no se por que se me importa este using solo al poner la interfaz.
 
 namespace inmobiliaria_lab2_pascual_leyes_delahoz_clavero.Models
 {
